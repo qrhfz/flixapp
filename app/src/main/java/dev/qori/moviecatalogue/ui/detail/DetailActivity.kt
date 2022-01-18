014be4844a276eb336a9ff2ff8acebbd48baca84
q <mboh.dong@gmail.com>
2022-01-18 11:15:58 +0700

    Initial commit

---
+++ app/src/main/java/dev/qori/moviecatalogue/ui/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package dev.qori.moviecatalogue.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.qori.moviecatalogue.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
