e31388594a0bc3cc0ae244e74cddf44e2370b79f
q <mboh.dong@gmail.com>
2022-01-19 11:16:11 +0700

    home view binding

--- app/src/main/java/dev/qori/moviecatalogue/ui/home/HomeActivity.kt
+++ app/src/main/java/dev/qori/moviecatalogue/ui/home/HomeActivity.kt
@@ -3,10 +3,14 @@ package dev.qori.moviecatalogue.ui.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dev.qori.moviecatalogue.R
+import dev.qori.moviecatalogue.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
